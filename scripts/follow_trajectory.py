954b5704aad34c056050689364811e23b7303873
ridhwan luthra <ridhwancode@gmail.com>
2017-06-09 02:43:57 +0530

    server's basic functionality ready, getting waypoints, need to make it move the drone

--- scripts/follow_trajectory.py
+++ scripts/follow_trajectory.py
@@ -16,19 +16,8 @@ pub = rospy.Publisher('/cmd_vel', Twist, queue_size=5)
 take_off_pub = rospy.Publisher('/ardrone/takeoff', Empty, queue_size=5)
 land_pub = rospy.Publisher('/ardrone/land', Empty, queue_size=5)
 
-# twist = Twist()
-# twist.linear.x = f[0]
-# twist.linear.y = f[1]
-# twist.angular.z
-
-waypoints = [[0,0,0,0,0,0,1], [1,0,0,0,0,0,1], [2,0,0,0,0,0,1], [3,0,0,0,0,0,1]]
-
-def moniter_transform(tf_listener):
-    try:
-        (trans,rot) = tf_listener.lookupTransform('/nav', '/base_link', rospy.Time(0))
-        return trans, rot
-    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        return None, None
+# waypoints = [[0,0,0,0,0,0,1], [1,0,0,0,0,0,1], [2,0,0,0,0,0,1], [3,0,0,0,0,0,1]]
+waypoints = [[0,0,0,0], [1,0,0,0], [2,0,0,0], [3,0,0,0]]
 
 first_time = True
 
@@ -46,6 +35,7 @@ def generate_trajectory(trans, rot, tf_listener):
                                       waypoint[5],
                                       waypoint[6]
                                       ))
+        # eular[2] is yaw
         twist = Twist()
         print(start_state[0] + waypoint[0], trans[0])
         while (start_state[0] + waypoint[0]) > trans[0]:
@@ -58,7 +48,17 @@ def generate_trajectory(trans, rot, tf_listener):
             print(start_state[0] + waypoint[0], trans[0])
 
 
+def send_goal(waypoint):
+    client = actionlib.SimpleActionClient('move_to_waypoint', drone_application.msg.moveAction)
 
+    client.wait_for_server()
+    print('server_found')
+
+    goal = drone_application.msg.moveGoal(waypoint)
+    client.send_goal(goal)
+
+    client.wait_for_result()
+    return client.get_result()
 
 
 def callback(data):
@@ -86,46 +86,25 @@ def callback(data):
     # print(len(data.trajectory[0].multi_dof_joint_trajectory.points))
     # print(type(data.trajectory[0].multi_dof_joint_trajectory.points[2].transforms[0]))
 
-def listener():
-    # In ROS, nodes are uniquely named. If two nodes with the same
-    # node are launched, the previous one is kicked off. The
-    # anonymous=True flag means that rospy will choose a unique
-    # name for our 'listener' node so that multiple listeners can
-    # run simultaneously.
-    rospy.init_node('follow_trajectory', anonymous=True)
-    tf_listener = tf.TransformListener()
 
-    client = actionlib.SimpleActionClient('move_to_waypoint', drone_application.msg.moveAction)
-
-    client.wait_for_server()
-    print('server_found')
-
-    goal = drone_application.msg.moveGoal(waypoints[1])
-    client.send_goal(goal)
-
-    client.wait_for_result()
-    print(client.get_result())
-    return
-
-    while not rospy.is_shutdown():
-        trans, rot = moniter_transform(tf_listener)
-        if trans is None:
-            continue
-        generate_trajectory(trans, rot, tf_listener)
-        break
-    # take_off_pub.publish()
-    # print('published take off')
-    # rospy.Subscriber("/ardrone/navdata", Navdata, moniter_navdata)
-    # rospy.Subscriber("/move_group/display_planned_path", DisplayTrajectory, callback)
-    # spin() simply keeps python from exiting until this node is stopped
-    # rospy.spin()
-
-# runs the listener function if the file is run as a script
 if __name__ == '__main__':
     try:
-        listener()
-    except KeyboardInterrupt:
-        print('interrupt')
-    finally:
-        twist = Twist()
-        pub.publish(twist)
+        rospy.init_node('follow_trajectory', anonymous=True)
+        tf_listener = tf.TransformListener()
+
+        send_goal(waypoints[1])
+
+        # while not rospy.is_shutdown():
+        #     trans, rot = moniter_transform(tf_listener)
+        #     if trans is None:
+        #         continue
+        #     generate_trajectory(trans, rot, tf_listener)
+        #     break
+        # take_off_pub.publish()
+        # print('published take off')
+        # rospy.Subscriber("/ardrone/navdata", Navdata, moniter_navdata)
+        # rospy.Subscriber("/move_group/display_planned_path", DisplayTrajectory, callback)
+        # spin() simply keeps python from exiting until this node is stopped
+        # rospy.spin()
+    except rospy.ROSInterruptException:
+        print('got exception')
--- scripts/move_to_waypoint.py
+++ scripts/move_to_waypoint.py
@@ -1,10 +1,10 @@
 #! /usr/bin/env python
-
+from __future__ import print_function
 import rospy
-
 import actionlib
-
 import drone_application.msg
+import tf
+import numpy as np
 
 class moveAction(object):
     # create messages that are used to publish feedback/result
@@ -13,23 +13,36 @@ class moveAction(object):
 
     def __init__(self, name):
         self._action_name = name
+        self.tf_listener = tf.TransformListener()
         self._as = actionlib.SimpleActionServer(self._action_name, drone_application.msg.moveAction, execute_cb=self.execute_cb, auto_start = False)
         self._as.start()
         print('server ready')
+
+    def moniter_transform(self):
+        trans = None
+        while trans is None:
+            try:
+                trans, rot = self.tf_listener.lookupTransform('/nav', '/base_link', rospy.Time(0))
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                continue
+        return trans, rot
+
+    def move_to_waypoint(self, waypoint):
+        trans, rot = self.moniter_transform()
+        euler = tf.transformations.euler_from_quaternion(rot)
+
+        start_state = np.array([trans[0], trans[1], trans[2], euler[2]])
+        # print(start_state)
+
       
     def execute_cb(self, goal):
         # helper variables
         success = True
         
-        # append the seeds for the fibonacci sequence
         self._feedback.difference = [1]
         
-        # publish info to the console for the user
-        print(type(self._feedback.difference))
-        # rospy.loginfo('giving feedback', self._feedback.difference)
-        
         # start executing the action
-        print(goal.waypoint)
+        self.move_to_waypoint(goal.waypoint)
         # check that preempt has not been requested by the client
         if self._as.is_preempt_requested():
             rospy.loginfo('%s: Preempted' % self._action_name)
@@ -43,9 +56,12 @@ class moveAction(object):
             self._result.error = [2]
             rospy.loginfo('%s: Succeeded' % self._action_name)
             self._as.set_succeeded(self._result)
-        
+
 if __name__ == '__main__':
-    rospy.init_node('move_to_waypoint')
-    server = moveAction(rospy.get_name())
-    print(rospy.get_name())
-    rospy.spin()
+    try:
+        rospy.init_node('move_to_waypoint')
+        server = moveAction(rospy.get_name())
+
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        print('got exception')
