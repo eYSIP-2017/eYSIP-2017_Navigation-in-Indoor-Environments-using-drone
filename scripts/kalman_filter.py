fixed units for navdata

scripts/kalman_filter.py
@@ -28,8 +28,7 @@ class pFilter(object):
 
     def predict(self, dt, speed_var, control_gain):
         self.state += control_gain
-        # I probably wont have to do /1000000, make sure to check
-        self.var += speed_var * dt * dt / 1000000
+        self.var += speed_var * dt * dt
 
 
 class pvFilter(object):
@@ -56,9 +55,6 @@ class pvFilter(object):
 
     # calculates prediction variance matrix based on gaussian acceleration as error.
     def predict_gaussion_accel(self, dt, acceleration_var, control_gains, cov_fac=1, speed_var_fac=1):
-        # may not need this
-        dt /= 1000
-
         G = np.eye(2)
         G[0,1] = dt
 
@@ -72,9 +68,6 @@ class pvFilter(object):
     # calculates prediction using the given uncertainty matrix
     # vars is var(0) var(1) covar(0,1)
     def predict(self, dt, vars, control_gains):
-        # may not need this
-        dt /= 1000
-
         G = np.eye(2)
         G[0,1] = dt
 
@@ -107,7 +100,11 @@ class extendedKalmanFilter(object):
         self.var_pose_observation_yaw = 3*3
 
     def __str__(self):
-        return "x: {}       y: {}       z: {}       yaw: {}".format(self.x.state[0], self.y.state[0], self.z.state[0], self.yaw.state[0])
+        return "x: {}       y: {}       z: {}       yaw: {}".format(np.around(self.x.state[0], 3),
+                                                                    np.around(self.y.state[0], 3),
+                                                                    np.around(self.z.state[0], 3),
+                                                                    np.around(self.yaw.state[0], 3)
+                                                                    )
 
     def odometry_update(self):
         pass
@@ -129,14 +126,11 @@ class extendedKalmanFilter(object):
 
         if dt <= 0:
             return
-        
-        # needed?
-        dt /= 1000000.0
 
 
         # predict roll, pitch, yaw
-        roll_control_gain = dt*c3*(c4 * max(-0.5, min(0.5, active_control.linear.y)) - self.roll.state)
-        pitch_control_gain = dt*c3*(c4 * max(-0.5, min(0.5, active_control.linear.x)) - self.pitch.state)
+        roll_control_gain = dt*c3*(c4 * max(-0.5, min(0.5, active_control.linear.x)) - self.roll.state)
+        pitch_control_gain = dt*c3*(c4 * max(-0.5, min(0.5, active_control.linear.y)) - self.pitch.state)
         yaw_speed_control_gain = dt*c5*(c6 * active_control.angular.z - self.yaw.state[1])
 
 

scripts/localisation.py
@@ -10,6 +10,7 @@ from tf.transformations import euler_from_quaternion
 from ardrone_autonomy.msg import Navdata
 from aruco_mapping.msg import ArucoMarker
 
+from time import time
 from pose import Pose
 from pid import pid
 import numpy as np
@@ -18,6 +19,7 @@ from kalman_filter import extendedKalmanFilter
 def navdata_callback(nav):
     ekf.roll.observe(nav.rotX, ekf.var_pose_observation_rp_imu)
     ekf.pitch.observe(nav.rotY, ekf.var_pose_observation_rp_imu)
+    # TODO: handle jump from -180 to 180
     ekf.yaw.observe_pose(nav.rotZ, 1*1)
     if ekf.yaw.prev is not None:
         ekf.yaw.observe_speed(nav.rotZ - ekf.yaw.prev, 1*1)
@@ -29,10 +31,10 @@ def navdata_callback(nav):
 
     ekf.x.observe_speed(vx_global, ekf.var_speed_observation_xy)
     ekf.y.observe_speed(vy_global, ekf.var_speed_observation_xy)
-    ekf.z.observe_pose(nav.altd, ekf.var_pose_observation_z_IMU)
+    ekf.z.observe_pose(nav.altd / 1000.0, ekf.var_pose_observation_z_IMU)
     if ekf.z.prev is not None:
-        ekf.z.observe_speed(nav.altd - ekf.z.prev, ekf.var_pose_observation_z_IMU)
-    ekf.z.prev = nav.altd
+        ekf.z.observe_speed(nav.altd/1000.0 - ekf.z.prev, ekf.var_pose_observation_z_IMU)
+    ekf.z.prev = nav.altd / 1000.0
 
 def aruco_callback(aru):
     euler = euler_from_quaternion((aru.global_camera_pose.orientation.x,
@@ -45,15 +47,18 @@ def aruco_callback(aru):
     ekf.x.observe_pose(aru.global_camera_pose.position.x, ekf.var_pose_observation_z_aruco)
 
     # check if eular 0 is correct!!!!!!
-    ekf.roll.observe(euler[0], ekf.var_pose_observation_rp_aruco)
-    ekf.pitch.observe(euler[2], ekf.var_pose_observation_rp_aruco)
+    ekf.roll.observe(np.degrees(euler[0]), ekf.var_pose_observation_rp_aruco)
+    ekf.pitch.observe(np.degrees(euler[2]), ekf.var_pose_observation_rp_aruco)
 
-    ekf.yaw.observe_pose(euler[1], ekf.var_pose_observation_yaw)
+    ekf.yaw.observe_pose(np.degrees(euler[1]), ekf.var_pose_observation_yaw)
     print('finished aruco')
 
 def make_prediction(active_control):
-    print('entering pred')
-    ekf.prediction(active_control, 1)
+    current_time = time()
+    dt = current_time - make_prediction.previous_time
+    ekf.prediction(active_control, dt)
+    make_prediction.previous_time = current_time
+    # print(dt)
     print(ekf)
 
 
@@ -64,6 +69,7 @@ if __name__ == "__main__":
     # aruco_front = bool(rospy.get_param('~aruco_front', 'true'))
     rospy.Subscriber("/ardrone/navdata", Navdata, navdata_callback)
     rospy.Subscriber('/aruco_poses', ArucoMarker, aruco_callback)
+    make_prediction.previous_time = time()
     rospy.Subscriber('/cmd_vel', Twist, make_prediction)
     
     drone_pose_pub = rospy.Publisher('/drone_pose', Float64, queue_size=1)