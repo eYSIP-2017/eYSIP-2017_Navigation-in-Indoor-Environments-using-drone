f197b0fe8e49e1f05b7879f23474ab27aed0cc1c
ridhwan luthra <ridhwancode@gmail.com>
2017-06-27 16:29:15 +0530

    storage of position data of whycon for neural net training

---
+++ scripts/store_formation_data.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from __future__ import print_function
+
+import rospy
+from drone_application.msg import pid_error
+
+file = open('4_5_6.txt', 'w')
+file.truncate()
+
+def store_to_file(data):
+    data = str(data)
+    file.write(data[8: len(data) - 3])
+    file.write('\n')
+
+try:
+    rospy.init_node('logging')
+    rospy.Subscriber('/whycon_pose', pid_error, store_to_file)
+    rospy.spin()
+
+finally:
+    file.close()
